5d4f5dd7a8a11b31573fa3f11db4997000433249
akashkumar809 <akash.kumar809@gmail.com>
2022-11-15 14:26:18 +0200

    add error log

--- src/main/java/com/demo/fizzbuzz/exception/FizzBuzzExceptionController.java
+++ src/main/java/com/demo/fizzbuzz/exception/FizzBuzzExceptionController.java
@@ -1,14 +1,17 @@
 package com.demo.fizzbuzz.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class FizzBuzzExceptionController {
 
   @ExceptionHandler(value = BadRequestException.class)
   public ResponseEntity<String> exception(BadRequestException exception) {
+    log.error(exception.getMessage(), exception);
     return new ResponseEntity<>(exception.getMessage(), exception.getStatus());
   }
 }
